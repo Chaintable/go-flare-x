a6a36b04c2b41c970dfc6dec2e59574605d0eaa4
Zhang Guyu <zhangguyu6@gmail.com>
2025-09-15 01:10:15 +0000

    fix

--- coreth/core/state_processor.go
+++ coreth/core/state_processor.go
@@ -96,7 +96,7 @@ func (p *StateProcessor) Process(block *types.Block, parent *types.Header, state
 	// Iterate over and process the individual transactions
 	var pipelineTracer *tracer.PipelineTracer
 	if p, ok := cfg.PipelineTracer.(*tracer.PipelineTracer); !ok {
-		log.Warn("vmConfig.Tracer must be a pipeline.Tracer")
+		log.Crit("vmConfig.PipelineTracer must be a pipeline.Tracer")
 	} else {
 		pipelineTracer = p
 		vmenv.Config.Tracer = pipelineTracer
